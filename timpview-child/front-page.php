446bbe645d24237627b205b4b7a0f8b8d84dc756
Josh Espinoza <joshespi@gmail.com>
2019-02-06 16:26:58 -0700

    changed front page to pull 9 posts instead of 18. Aslo made it so the new principals message post type will pull into the same blog roll

--- timpview-child/front-page.php
+++ timpview-child/front-page.php
@@ -17,14 +17,14 @@
 							<?php endwhile;
 					wp_reset_query();
 				?>
-				<button id="closeAlert"><img src="https://timpview.provo.edu/wp-content/themes/pcsd-2018-theme/assets/icons/round-delete-button-white.svg" alt="Close Alerts" /></button>
+				<button id="closeAlert"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/round-delete-button-white.svg" alt="Close Alerts" /></button>
 			</section>
 			<?php } ?>
-			<section id="announcments">
+			 <section id="announcments">
 				<h1>Timpview Announcements</h1>
 					 <div class="slick-wrapper">
 					  <?php
-					  		$args = array( 'post_type' => 'announcement' ,'posts_per_page' => 5 , 'orderby'  => array('date' => 'DESC'));
+					  		$args = array( 'post_type' => 'announcement' ,'posts_per_page' => 8 , 'orderby'  => array('date' => 'DESC'));
 							// Variable to call WP_Query.
 							$the_query = new WP_Query( $args );
 							if($the_query->have_posts()) :
@@ -55,16 +55,20 @@
 			<section id="mainContent" class="postgrid newsBlog">
 				<h1>Timpview News</h1>
 					<?php
+						
 						$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-						$the_query = new WP_Query( array( 'posts_per_page' => 18 , 'post_type'  => 'post' , 'paged'  => $paged) );
+						$the_query = new WP_Query( array( 'posts_per_page' => 18 , 'post_type'  => array('post', 'principals_message') , 'paged'  => $paged) );
 						if($the_query->have_posts()) :
 							while ($the_query->have_posts()) : $the_query->the_post();?>
 						   		<article class="post">
-							   		
-							   		
 					   				<header class="postmeta">
 						   				<?php 
-							   				if(has_post_thumbnail()) { ?>
+							   				if ($post->post_type == "principals_message") { ?>
+							   					<a href="<?php the_permalink(); ?>">
+						   							<div class="featured-image">
+						   								<img src="<?php echo get_theme_file_uri(); ?>/assets/images/principal-message.jpg" alt="Admin photo" class="left" />
+								   					</div>
+ 									<?php	} elseif (has_post_thumbnail()) { ?>
 						   						<a href="<?php the_permalink(); ?>">
 						   							<div class="featured-image">
 						   								<?php the_post_thumbnail(); ?>
@@ -73,11 +77,11 @@
 							   		<?php } else { ?>
 								   				<a href="<?php the_permalink(); ?>">
 												   	<div class="featured-image">
-											   			<img src="<?php echo get_stylesheet_directory_uri().'/assets/images/timpview-logo.png'; ?>" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="" width="217" height="175">
+											   			<img src="<?php echo get_stylesheet_directory_uri().'/assets/images/header-logo.png'; ?>" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="" width="217" height="175">
 											   		</div>
 							   					
 							   		<?php }?>
-													<h2><?php the_title(); ?></h2>
+													<h2><?php if ($post->post_type == "principals_message") { echo "Principal's Message - " . get_the_title(); } else { the_title(); } ?></h2>
 								   				</a>
 										<ul>
 											<li><img src="//globalassets.provo.edu/image/icons/calendar-ltblue.svg" alt="" /><?php the_time(' F jS, Y') ?></li>
