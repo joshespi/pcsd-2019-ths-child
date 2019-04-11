d1d5e7b669f4ec1b17710295185ab384da2ede42
Josh Espinoza <joshespi@gmial.com>
2019-04-11 16:15:16 -0600

    purged style sheet

--- timpview-child/functions.php
+++ timpview-child/functions.php
@@ -6,7 +6,7 @@ function my_theme_enqueue_styles() {
 
     $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css', '1.0.1', true );
     wp_enqueue_script( 'child_scripts', get_theme_file_uri().'/assets/js/child-scripts.js', '', '1.0', true);
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
@@ -20,14 +20,13 @@ Custom Excerpt
 ============================================================================================*/
 function get_excerpt(){
 	$excerpt = get_the_content();
-	$excerpt = preg_replace(" ([.*?])",'',$excerpt);
+	//$excerpt = preg_replace(" ([.*?])",'',$excerpt);
 	$excerpt = strip_shortcodes($excerpt);
 	$excerpt = strip_tags($excerpt);
 	$excerpt = substr($excerpt, 0, 200);
 	$excerpt = substr($excerpt, 0, strripos($excerpt, " "));
 	$excerpt = trim(preg_replace( '/\s+/', ' ', $excerpt));
 	$excerpt = '<p>'.$excerpt.'...'.'</p>';
-
 	return $excerpt;
 }
 
