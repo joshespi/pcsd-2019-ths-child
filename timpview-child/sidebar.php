380f176f0ae6c58ddc973db241f7f398662994e2
Josh Espinoza <joshespi@gmial.com>
2019-12-04 10:57:45 -0700

    added link to canvas sites

--- timpview-child/sidebar.php
+++ timpview-child/sidebar.php
@@ -121,6 +121,12 @@
 							<span>Solicitud de exención de cuotas</span>
 						</a>
 					</li>
+					<li>
+						<a href="https://provo.instructure.com/login/saml/14">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/file-white.svg" alt="" />
+							<span>Connect to Canvas (Online Classroom)</span>
+						</a>
+					</li>
 				</ul>
 				<a href="https://www.peachjar.com/index.php?region=33081&a=28&b=138"><img src="https://timpview.provo.edu/wp-content/uploads/2019/05/button-orange-eflyers_202x46.png" alt="Link to PeachJar Fliers"></a>
 				<a href="https://safeut.med.utah.edu/"><img src="https://timpview.provo.edu/wp-content/uploads/2019/03/safeUTcrisisline.jpg" alt="Link to SafeUT information"></a>
